8f24a370a1adc5b8a1a4e9a524b764cf1d9a209c
Arthur-Cunhax <arthurcl634@gmail.com>
2025-08-03 01:59:55 -0300

    commit

--- Platformer.py
+++ Platformer.py
@@ -6,6 +6,16 @@ from pygame import Rect
 WIDTH = 800
 HEIGHT = 600
 
+
+
+start_button = Actor("start")
+start_button.x = WIDTH // 2
+start_button.y = 300
+
+exit_button = Actor("exit")
+exit_button.x = WIDTH // 2 
+exit_button.y = 500
+
 background = Actor("background")
 sound = Actor("mute")
 sound.x = 20
@@ -18,29 +28,64 @@ gravity = 1.25
 jump_force = -25
 floor = 550
 
+
 state = "startscreen"
 
-def iniciarJogo():
-    global state
-    if keyboard[keys.SPACE] and state == "startscreen":
-        generate_platforms()
-        coin_generate()
-        sounds.click.play()
-        state = "game"
+
 
 main_character = Actor("principal/principal")
-main_character.x = 400
+main_character.x = 650
 main_character.y = 550
 
-enemy_one = Actor("enemies/saw")
+enemy_one = Actor("enemies/saw_sprites/saw_a")
 enemy_one.x = 50
 enemy_one.y = 340
+saw_frame = 0
 
 
-enemy_two = Actor("enemies/mouse")
+saw_sprites = [
+    "enemies/saw_sprites/saw_a", "enemies/saw_sprites/saw_b", "enemies/saw_sprites/saw_c",
+    "enemies/saw_sprites/saw_d", "enemies/saw_sprites/saw_e", "enemies/saw_sprites/saw_f",
+    "enemies/saw_sprites/saw_g", "enemies/saw_sprites/saw_h", "enemies/saw_sprites/saw_i",
+    "enemies/saw_sprites/saw_j", "enemies/saw_sprites/saw_k", "enemies/saw_sprites/saw_l"
+]
+
+def saw_animation():
+    global saw_frame
+    enemy_one.image = saw_sprites[saw_frame]
+    saw_frame = (saw_frame + 1) % len(saw_sprites)
+
+clock.schedule_interval(saw_animation, 0.2)
+
+
+enemy_two = Actor("enemies/mouse/mouse_a")
 enemy_two.x = 610
 enemy_two.y = 182
 enemy_two_direction = -1
+mouse_frame = 0
+
+mouse_sprites = [
+    "enemies/mouse/mouse_a",
+    "enemies/mouse/mouse_b",
+    "enemies/mouse/mouse_c"]
+
+mouse_flipped_sprites = [
+    "enemies/mouse/mouse_flipped_a",
+    "enemies/mouse/mouse_flipped_b",
+    "enemies/mouse/mouse_flipped_c"
+]
+
+def mouse_animation():
+    global mouse_frame , enemy_two_direction
+    if enemy_two_direction == -1:
+        enemy_two.image = mouse_sprites[mouse_frame]
+        mouse_frame = (mouse_frame + 1) % len(mouse_sprites)
+    elif enemy_two_direction == 1:
+        enemy_two.image = mouse_flipped_sprites[mouse_frame]
+        mouse_frame = (mouse_frame + 1) % len(mouse_flipped_sprites)
+
+clock.schedule_interval(mouse_animation, 0.2)
+
 
 platforms = []
 platforms_xy = [
@@ -91,20 +136,31 @@ def check_victory():
     if len(coins) == 0 and state == "game":
         main_character.image = "principal/victory"
         state = "victory"
+        sounds.victory.play()
+
     
 clock.schedule_interval(coin_flip, 0.5)
 
 def on_mouse_down(pos,button):
+    global state, coin_count
     if sound.collidepoint(pos) and button == mouse.LEFT:
         if sound.image == "mute":
             sound.image = "unmute"
         else:
             sound.image = "mute"
+    if start_button.collidepoint(pos) and button == mouse.LEFT:
+        state = "game"
+        sounds.click.play()
+        generate_platforms()
+        coin_generate()
+    if exit_button.collidepoint(pos) and button == mouse.LEFT:
+        exit()
+
 
 def song_play():
     global song_playing 
     if sound.image == "mute" and not song_playing:
-        sounds.song.play()
+        sounds.song.play(-1)
         sounds.song.set_volume(0.08)
         song_playing = True
     elif sound.image == "unmute":
@@ -114,12 +170,10 @@ def song_play():
         sounds.song.stop()
 
 
-
-
 def check_platform_colision():
-    global jumping, vertical_speed
+    global jumping, vertical_speed, state
     onplatform = False
-
+    
     if vertical_speed >= 0: 
         for platform in platforms:
             character_bottom = main_character.y + main_character.height // 2
@@ -169,10 +223,10 @@ def mouse_movement():
 
     if enemy_two.x >= 730:
         enemy_two_direction = -1
-        enemy_two.image = "enemies/mouse"
+        enemy_two.image = "enemies/mouse/mouse_a"
     elif enemy_two.x <= 480:
         enemy_two_direction = 1
-        enemy_two.image = "enemies/mouse_flipped"
+        enemy_two.image = "enemies/mouse/mouse_flipped_a"
 
 
 
@@ -182,44 +236,43 @@ def update():
     if state == "gameover" or state =="victory":
         return
     song_play()
-    iniciarJogo()
+
+    if state == "game":
+        if keyboard.left:
+            main_character.x -= 5
+        if keyboard.right:
+            main_character.x += 5
+        if main_character.x < 35:
+            main_character.x = 35
+        if main_character.x > WIDTH - 35:
+            main_character.x = WIDTH - 35
 
 
-    if keyboard.left:
-        main_character.x -= 5
-    if keyboard.right:
-        main_character.x += 5
-    if main_character.x < 35:
-        main_character.x = 35
-    if main_character.x > WIDTH - 35:
-        main_character.x = WIDTH - 35
+        if not jumping and keyboard.space:
+            jumping = True
+            vertical_speed = jump_force
+            sounds.jump.play()
+            sounds.jump.set_volume(0.3)
 
+        
+        check_platform_colision()
+        saw_animation()
 
-    if not jumping and keyboard.space:
-        jumping = True
-        vertical_speed = jump_force
-        sounds.jump.play()
-        sounds.jump.set_volume(0.3)
+        if jumping:
+            main_character.image = "principal/jumping"
+            main_character.y += vertical_speed
+            vertical_speed += gravity
 
-    
-    check_platform_colision()
+            if main_character.y >= floor:
+                main_character.y = floor
+                jumping = False
+                vertical_speed = 0 
+        
+        
 
-
-    if jumping:
-        main_character.image = "principal/jumping"
-        main_character.y += vertical_speed
-        vertical_speed += gravity
-
-        if main_character.y >= floor:
-            main_character.y = floor
-            jumping = False
-            vertical_speed = 0 
-    
-    
-
-    if not jumping:
-        main_character.image = "principal/principal"
-
+        if not jumping:
+            main_character.image = "principal/principal"
+        
 
 
     check_coin_colision()
@@ -233,7 +286,8 @@ def draw():
 
     if state == "startscreen":
         screen.draw.text("Meu Jogo", center=(WIDTH // 2,100), fontsize=100)
-        screen.draw.text("Pressione Espaço para iniciar o jogo", center=(WIDTH// 2 , 300), fontsize=50)
+        start_button.draw()
+        exit_button.draw()
         sound.draw()
 
 
